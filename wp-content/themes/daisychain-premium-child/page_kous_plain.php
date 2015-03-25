1e38604b23b8feabb1fecb3b625bbde383275633
lisajwells <lisawells999@gmail.com>
2015-03-25 13:52:09 -0400

    adding font and color reference pds

---
+++ wp-content/themes/daisychain-premium-child/page_kous_plain.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The page template file.
+ * Template Name: Kousouros1 Page
+ * Description: A Page Template for non blogging.
+*/
+
+get_header(); ?>
+<?php daisychain_get_display_image_page(); ?>
+    <div class="entry-content">
+<?php the_content(); ?>
+  </div> <!-- end of content -->
+<?php get_footer(); ?>
+
+
+
