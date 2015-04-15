7c951dd4a7096e494edac25f1d4bc59b8e7e268e
lisajwells <lisawells999@gmail.com>
2015-04-14 20:10:47 -0400

    header image and trying bones theme

--- wp-content/themes/naked-child/header.php
+++ wp-content/themes/naked-child/header.php
@@ -44,28 +44,25 @@
 >
 
 <header id="masthead" class="site-header" role="banner">
+	<div class="container center">
+	
+		<nav role="navigation" class="site-navigation main-navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
+		</nav><!-- .site-navigation .main-navigation -->
+	</div>
 	<div class="center">
 
-			<a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"> 
 		<div id="brand">
 			<h1 class="site-title">
-				<?php bloginfo( 'name' ); // Display the blog name ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
 			</h1>
-		</div><!-- /brand -->
-		</a>
-
-			<div class="container center">
-				<nav role="navigation" class="site-navigation main-navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
-				</nav><!-- .site-navigation .main-navigation -->
-			</div><!-- container center -->
-			
 			<h4 class="site-description">
 				<?php bloginfo( 'description' ); // Display the blog description, found in General Settings ?>
 			</h4>
+		</div><!-- /brand -->
 		
 		<div class="clear"></div>
-	</div><!-- center-->
+	</div><!--/container -->
 		
 </header><!-- #masthead .site-header -->
 
