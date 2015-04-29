6b9181218637d6bc7e98fe616bbe6eb1dc8141be
lisajwells <lisawells999@gmail.com>
2015-04-29 13:36:37 -0400

    the download_pdf_file_url field is downloading the pdf

--- wp-content/themes/flat-bootstrap-child/page-press.php
+++ wp-content/themes/flat-bootstrap-child/page-press.php
@@ -23,24 +23,123 @@ get_header(); ?>
 
         <?php while ( have_posts() ) : the_post(); ?>
 
-          <article id="post-<?php the_ID(); ?>" <?php post_class( 'testimonial_loop' ); ?>>
+          <article id="post-<?php the_ID(); ?>" <?php post_class( 'press_loop' ); ?>>
 
-            <div class="entry-content">
+						<div class="row">
+	            <div class="col-md-10 col-md-offset-1 press-subhed">
+	            <h3>Print</h3>
+	          </div>
+	          </div>
+
+						<div class="row">
+
+            <div class="entry-content-press1 col-md-5 col-md-offset-1"><!-- DailyNews, NYT, Journal News-->
                 <?php the_content(); ?>
                 <?php 
 
-                $press_query = new WP_Query( 'category_name=press');
+                $press_print1_query = new WP_Query( 'category_name=daily-news');
+                $press_print2_query = new WP_Query( 'category_name=new-york-times');
+                $press_print3_query = new WP_Query( 'category_name=journal-news');
 
-                while ( $press_query->have_posts() ) : $press_query->the_post(); ?>
+                ?>
 
-                    <p><?php the_content(); ?></p>
-                    <?php  the_title();  ?></p>
+
+								<h4>Daily News</h4>
+
+                <?php while ( $press_print1_query->have_posts() ) : $press_print1_query->the_post(); ?>
+
+                    <p class="print-date"><?php  the_date();  ?></p>
+                    <p class="print-title"><?php  the_title();  ?></p>
 
                 <?php endwhile; // end of the loop. ?>
 
-                <?php edit_post_link( __( '<span class="glyphicon glyphicon-edit"></span> Edit', 'flat-bootstrap' ), '<footer class="entry-meta"><div class="edit-link">', '</div></footer>' ); ?>
+								<h4>New York Times</h4>
+
+                <?php while ( $press_print2_query->have_posts() ) : $press_print2_query->the_post(); ?>
+
+                    <p class="print-date"><?php  the_date();  ?></p>
+                    <p class="print-title"><?php  the_title();  ?></p>
+
+                <?php endwhile; // end of the loop. ?>
+
+								<h4>Journal News</h4>
+
+                <?php while ( $press_print3_query->have_posts() ) : $press_print3_query->the_post(); ?>
+
+                    <p class="print-date"><?php  the_date();  ?></p>
+                    <p class="print-title"><?php  the_title();  ?></p>
+                    
+ <?php if( get_field( 'download_pdf_file_url') ): ?>
+    <a target="_blank" href="<?php the_field('download_pdf_file_url'); ?>">Télécharger le PDF</a>
+      <?php else: ?>
+     <?php endif; ?>
+
+
+                <?php endwhile; // end of the loop. ?>
+
 
             </div><!-- .entry-content -->
+
+
+            <div class="entry-content-press2 col-md-5"><!-- Newsday, TheTimesOfSmithtown -->
+                <?php the_content(); ?>
+                <?php 
+
+                $press_print4_query = new WP_Query( 'category_name=newsday');
+                $press_print5_query = new WP_Query( 'category_name=times-of-smithtown');
+
+                ?>
+
+								<h4>Newsday</h4>
+
+                <?php while ( $press_print4_query->have_posts() ) : $press_print4_query->the_post(); ?>
+
+                    <p class="print-date"><?php  the_date();  ?></p>
+                    <p class="print-title"><?php  the_title();  ?></p>
+
+                <?php endwhile; // end of the loop. ?>
+
+								<h4>Times of Smithtown</h4>
+
+                <?php while ( $press_print5_query->have_posts() ) : $press_print5_query->the_post(); ?>
+
+                    <p class="print-date"><?php  the_date();  ?></p>
+                    <p class="print-title"><?php  the_title();  ?></p>
+
+                <?php endwhile; // end of the loop. ?>
+
+
+            </div><!-- .entry-content -->
+
+          </div><!-- .row Print -->
+
+						<div class="row">
+	            <div class="col-md-10 col-md-offset-1 press-subhed">
+	            <h3>Cable and Network Appearances</h3>
+	          </div>
+	          </div>
+
+
+          <div class="row"><!-- Here starts the TV section -->
+          	
+            <div class="entry-content-press3 col-md-5 col-md-offset-1">
+
+                <?php 
+
+                $press_tv1_query = new WP_Query( 'category_name=TV');
+
+                ?>
+
+                <?php while ( $press_tv1_query->have_posts() ) : $press_tv1_query->the_post(); ?>
+
+                    <p><?php  the_title();  ?></p>
+                    <p><?php the_content(); ?></p>
+
+                <?php endwhile; // end of the loop. ?>
+
+              </div><!-- .entry-content-press3 -->
+
+          </div><!-- .row TV -->
 
           </article><!-- #post-## -->
 
