c3e673afc1db17248ec9a88863ff6faf823b4007
lisajwells <lisawells999@gmail.com>
2015-04-30 10:33:30 -0400

    avvo javascript script tag moved to header.php

--- wp-content/themes/flat-bootstrap-child/header.php
+++ wp-content/themes/flat-bootstrap-child/header.php
@@ -16,6 +16,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <script type="text/javascript" src="http://www.avvo.com/assets/badges-v2.js"></script>
 
 <?php wp_head(); ?>
 </head>
