a608e444bdc44798d9aa199c0a79db637fa01c78
lisajwells <lisawells999@gmail.com>
2015-04-30 14:43:46 -0400

    added conditional external link to print-press title in NYT

--- wp-content/themes/flat-bootstrap-child/page-press.php
+++ wp-content/themes/flat-bootstrap-child/page-press.php
@@ -56,7 +56,13 @@ get_header(); ?>
 							<?php while ( $press_print2_query->have_posts() ) : $press_print2_query->the_post(); ?>
 
 								<p class="press-date"><?php  the_date();  ?></p>
-								<p class="press-title"><?php  the_title();  ?></p>
+
+								<?php if( get_field( 'external_link') ): ?>
+									<p class="press-title"><a target="_blank"  class="press-pdf" href="<?php the_field('external_link'); ?>"><?php  the_title();  ?></a></p>
+								<?php else: ?>
+									<p class="press-title"><?php  the_title();  ?></p>
+								<?php endif; ?>
+
 								<?php if( get_field( 'download_pdf_file_url') ): ?>
 									<a target="_blank"  class="press-pdf" href="<?php the_field('download_pdf_file_url'); ?>">Download PDF</a>
 								<?php else: ?>
