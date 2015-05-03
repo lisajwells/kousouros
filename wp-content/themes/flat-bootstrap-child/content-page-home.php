fe46b8a34980f362356e7134b89f7b43e6676806
Lisa Wells <lisawells999@gmail.com>
2015-05-03 14:20:47 -0400

    testimonials section populating with post loop

--- wp-content/themes/flat-bootstrap-child/content-page-home.php
+++ wp-content/themes/flat-bootstrap-child/content-page-home.php
@@ -1,6 +1,17 @@
-<div>
-[metaslider id=1471]
-</div>
+<?php
+/**
+ * Theme: Flat Bootstrap
+ * 
+ * The template used for displaying page content for the home page. It contains 
+ * everything after the_content()
+ *
+ * @package flat-bootstrap
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<div class="entry-content">
 
 <div class="section"><!-- specializing and logos -->
   <div class="container">
@@ -27,30 +38,33 @@
         <img class="alignnone size-full wp-image-904" src="http://localhost:8888/kousouros_law/wp-content/uploads/2013/03/image-alignment-150x150.jpg" alt="Image Alignment 150x150" width="150" height="150" /></div>
     </div><!-- .row -->
 
-  </div><!-- .container-fluid -->
+  </div><!-- .container -->
 </div><!-- .section specializing and logos-->
 
+<!-- HOME TESTIMONIALS SECTION -->
 <div class="section bg-darkgray" id="home-testimonials"><!-- home testimonials -->
   <div class="container">
-    <h2>What clients are saying about James Kousouros</h2>
+
+	<h2><?php the_field('home_testimonials_title'); ?></h2>    
     <div class="row"><!--  -->
 
+<?php $tesimonial_query = new WP_Query( 'category_name=testimonials');  ?>
+
+	<?php while ( $tesimonial_query->have_posts() ) : $tesimonial_query->the_post(); ?>
       <div class="col-md-4">
-        <p>Bacon ipsum dolor amet shankle pig pork loin frankfurter tail brisket, alcatra doner tongue. Pork belly picanha frankfurter brisket, tail jerky beef ribs. Tenderloin biltong fatback, meatball tail andouille hamburger prosciutto ribeye. Beef biltong flank cow pancetta fatback pastrami.</p>
+		<p><?php the_excerpt(); ?></p>
       </div><!-- col-4 -->
-      <div class="col-md-4">
-        <p>Bacon ipsum dolor amet shankle pig pork loin frankfurter tail brisket, alcatra doner tongue. Pork belly picanha frankfurter brisket, tail jerky beef ribs. Tenderloin biltong fatback, meatball tail andouille hamburger prosciutto ribeye. Beef biltong flank cow pancetta fatback pastrami.</p>
-      </div><!-- col-4 -->
-      <div class="col-md-4">
-        <p>Bacon ipsum dolor amet shankle pig pork loin frankfurter tail brisket, alcatra doner tongue. Pork belly picanha frankfurter brisket, tail jerky beef ribs. Tenderloin biltong fatback, meatball tail andouille hamburger prosciutto ribeye. Beef biltong flank cow pancetta fatback pastrami.</p>
-      </div><!-- col-4 -->
+	<?php endwhile; // end of the loop. ?>
 
     </div><!-- .row -->
+
+    <div class="row" id="home-testimonials-button">
         <p style="text-align: center;"><a class="btn btn-hollow" href="#">Read More Testimonials</a></p>
+    </div><!-- row -->
   </div><!-- .container -->
-</div><!-- section testimonials and representing -->
+</div><!-- section testimonials -->
 
-
+<!-- HOME REPRESENTS SECTION -->
 <div class="section" id="home-represents"><!-- home represents -->
   <div class="container">
 
@@ -65,7 +79,7 @@
             <p style="text-align: center;"><a class="btn btn-hollow-blackborder" href="#">Watch More Videos</a></p>
           </div>
           <div class="col-md-9">
-            <h2>Mr. Kousouros talks about representing his clients</h2>
+			<h2><?php the_field('home_represents_title'); ?></h2>    
             <p>Bacon ipsum dolor amet shankle pig pork loin frankfurter tail brisket, alcatra doner tongue. Pork belly picanha frankfurter brisket, tail jerky beef ribs. Tenderloin biltong fatback, meatball tail andouille hamburger prosciutto ribeye. Beef biltong flank cow pancetta fatback pastrami.</p>
           </div>
 </div><!-- row-->
@@ -79,7 +93,7 @@
 </div><!-- section home-represents -->
 
 
-
+<!-- HOME CONSULTATION SECTION -->
 <div class="section bg-darkgray center" id="home-consultation"><!-- home consultation -->
   <div class="container">
     <div class="row">
@@ -108,10 +122,11 @@
   </div><!-- container -->
 </div><!-- section home-consulation -->
 
+<!-- HOME PRESS SECTION -->
 <div class="section center" id="home-press"><!-- home press -->
   <div class="container">
     <div class="row">
-      <h2>In the News</h2>
+		<h2><?php the_field('home_press_title'); ?></h2>    
       <p>Mr. Kousouros is a frequent guest as a legal commentator on various network and cable stations.<p>
       <div class="col-md-3">
         <figure>
@@ -143,13 +158,13 @@
   </div><!-- container -->
 </div><!-- section home-press -->
 
-
+<!-- HOME NOTABLES SECTION -->
 <div class="section bg-darkgray" id="home-notable"><!-- home notable -->
   <div class="container">
     <div class="row">
       
       <div class="col-md-6">
-        <h2>Vigorous and Effective Representation</h2>
+		<h2><?php the_field('home_notables_title'); ?></h2>    
         <p>Bacon ipsum dolor amet shankle pig pork loin frankfurter tail brisket, alcatra doner tongue. Pork belly picanha frankfurter brisket, tail jerky beef ribs. Tenderloin biltong fatback, meatball tail andouille hamburger prosciutto ribeye. Beef biltong flank cow pancetta fatback pastrami.</p>
         <p style="text-align: center;"><a class="btn btn-hollow-blackborder" href="#">Read more notable cases</a></p>
       </div><!-- col-6 -->
@@ -180,9 +195,11 @@
   </div><!-- container-->
 </div><!-- section home-notable-->
 
+<!-- HOME NATIONAL AND INTERNATIONAL SECTION -->
 <div class="section" id="home-national"><!-- home-national -->
   <div class="container">
-    <h3>National and International</h3>
+	<h3><?php the_field('home_national_title'); ?></h3>    
+
     <div class="row">
       <div class="col-md-4 col-md-offset-2">
           <img class="alignnone size-full wp-image-904 aligncenter" src="http://localhost:8888/kousouros_law/wp-content/uploads/2013/03/image-alignment-150x150.jpg" alt="Image Alignment 150x150" width="150" height="150" />
@@ -195,3 +212,7 @@
   </div><!-- container -->
 </div><!-- section home-national -->
 
+		
+</div><!-- .entry-content -->
+	
+</article><!-- #post-## -->
