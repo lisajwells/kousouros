4ad80bf187055db4525e6d64b44391218e6671ab
Lisa Wells <lisawells999@gmail.com>
2015-05-14 18:38:28 -0400

    adding content to ThePractice, Profile

--- wp-content/themes/flat-bootstrap-child/scratch.php
+++ wp-content/themes/flat-bootstrap-child/scratch.php
@@ -1,4 +1,4 @@
-Firm Overview DKK edit
+***Firm Overview DKK edit
 <p class="intro-graph"><b>Located in midtown Manhattan,</b> the Law Office of James Kousouros handles all types of criminal prosecutions under state and federal laws. These cases include prosecutions for white-collar crimes such as complex securities violations, Medicaid, Medicare and healthcare fraud, pharmaceutical industry fraud, and tax fraud.  They also include charges for racketeering, homicide, narcotics violations, DWI, and simple assault, among many others. (See, Notable Cases).</p>  
 <h3>All Stages of Representation</h3>
 <p>As a full service criminal practice, we represent clients in all stages of the criminal process:</p> 
@@ -19,9 +19,92 @@ Firm Overview DKK edit
 <p>Mr. Kousouros not only believes in frequent client contact, he insists on it. This insistence comes from his firm belief that a client’s participation in his or her own defense is crucial —not only to ensure that every case is properly and thoroughly investigated, but to ensure that his client fully understands and considers all of the options available. For Mr. Kousouros, the benefits of full client participation are essential to his firm’s goal of achieving the best resolution possible.</p>
 
 
+***Services Provided
+<h3>Personalized Attention</h3> 
+<p>If you, or someone you know, is facing or fearing a criminal investigation or prosecution, you need a criminal defense attorney who is:</p> 
+<ul>
+<li>trustworthy</li>
+<li>an expert in the field</li>
+<li>attentive and compassionate</li>
+</ul>
+<p>You also need someone you are convinced will:</p>
+<ul>
+<li>protect you</li>
+<li>fight zealously and aggressively for you</li>
+<li>be the person you want to have by your side throughout the proceedings</li>
+</ul>
+<p>James Kousouros honors these qualities, and is committed to providing them to each and every one of his clients.</p>
+<h3>Crafting Resolutions with Your Needs in Mind</h3>
+<p>Mr. Kousouros understands how disruptive criminal charges can be for an individual — both personally and professionally — and he will do everything possible to protect:</p> 
+<ul>
+<li>your professional license </li>
+<li>your employment status</li>
+<li>your immigration status</li>
+</ul>
+<h3>The Benefits of a Small Firm, with Advantages of a Large One</h3>
+<p>The Law Offices of James Kousouros offers you the benefits of a small firm – a personalized and comfortable environment where your needs are paramount — with the same legal resources available to the larger firms:</p> 
+<ul>
+<li>private investigators and bail bondsmen </li>
+<li>forensic experts </li>
+<li>interpreters </li>
+<li>the latest in electronic databases for court documents and case data </li>
+</ul>
+<h3>James Kousouros’ Promise to his Clients</h3>
+<p>Mr. Kousouros will promise to fight - vigorously, relentlessly, strategically, and effectively - at every stage of your defense. He will use everything at his disposal</p>  
+<ul>
+<li>his deep knowledge of the law </li>
+<li>his thorough command of the intricacies of the criminal justice system</li>
+<li>his familiarity with the theories and tactics of the prosecutors </li>
+<li>the results of his thorough investigation into the facts and circumstances of your case</li>
+</ul>
+<p>to develop a strategy that will take advantage of every opportunity that arises during negotiations, the pretrial process, and at trial.</p>
 
+***The Process
+<p class="process1"><strong>Free Consultation</strong> In most cases, Mr. Kousouros will provide you with a free consultation during which he will assess your situation. First, he will determine if you need a lawyer, and then he will determine if he is the right one for you. Then he will spend time reviewing your case and discussing it with you. These conversations will be substantive ones, not sales pitches. He will ask you to bring your paperwork with you to the meeting or send it to him in advance.  If the case is electronically filed he will review it prior to meeting with you.</p> 
+<p><strong>What you can expect</strong> An honest and direct appraisal of your criminal case. You will hear what you need to hear in order to make an informed decision that is rooted in the realities of your case. You will not hear promises nor receive any guarantees of particular outcomes— no lawyer can or should provide such assurances. You will hear the truth.</p>
+<p class="process1"><strong>Pre-Arrest</strong> In those cases where the client is under investigation and has not been arrested, Mr. Kousouros will contact authorities to ensure that his client is not questioned unless he is at his client’s side. He will review the facts with the client and often commence his own investigation in order to gather evidence to assist in his efforts to prevent an arrest— an outcome he has often achieved. If an arrest is unavoidable, Mr. Kousouros will exercise his best efforts to arrange for a voluntary surrender and spare his client the indignity of a public arrest.</p> 
+<p><strong>What you can expect</strong> The benefit of his long and mutually respectful relationships with both law enforcement and prosecutorial agencies. This is not a time to be antagonistic – it is a time to respectfully discuss a client’s situation with the authorities and to do everything possible to ensure they will be willing to listen.</p> 
+<p class="process1"><strong>Investigation</strong> Mr. Kousouros will immediately conduct meetings with his client in order to get a full and complete account of who his client is and the circumstances surrounding the criminal charges. Every case involves these two components—the case and the person. If it turns out that the case is too strong, mitigation is often found in the person.</p> 
+<p><strong>What you can expect</strong> Mr. Kousouros will take all necessary steps and precautions to ensure that evidence is preserved, leads are investigated, witnesses are located and interviewed, and all evidence is examined. He will also visit the scene – a step that is critical to an effective defense. These efforts will allow him to identify both the strengths and the weaknesses in the prosecution’s case—a task indispensable to pre-trial hearings, trial preparation and plea negotiations.</p> 
+<p class="process1"><strong>Negotiations</strong> The results of Mr. Kousouros’ thorough investigation, together with his expertise in the law and mastery of the criminal process, combine to place him in a very strong position during plea negotiations</p>
+<p><strong>What you can expect</strong> An advocate who has the respect and ear of prosecuting authorities as well as a partner who will do everything in his power to obtain the best resolution possible.</p> 
+<p class="process1"><strong>Pre-trial Motions</strong> Mr. Kousouros will use his considerable experience to strategically navigate the pre-trial process in a manner that safeguards all of his client’s rights and best interests. He will assert all standard legal rights of his client in his motion papers, but he will also use his creativity and dexterity to devise novel arguments and positions where appropriate.</p> 
+<p><strong>What you can expect</strong> Mr. Kousouros’ motions are, like all other areas of his practice, of the highest quality and consistently earn the praise of judges and prosecutors alike.</p> 
+<p class="process1"><strong>Trial</strong> Having tried scores of cases in both state and federal courts, Mr. Kousouros has developed and mastered the most effective strategies for all aspects of trial litigation—from jury selection, opening argument, and courtroom presentation to cross examination and closing argument. <a href="http://localhost:8888/kousouros_law/profile/">(Profile).</a></p>  
+<p><strong>What you can expect</strong> Complete confidence that you have received what you deserve: a zealous, effective and strategic defense.</p>
 
+***Profile Edit
+<p class="intro-graph">James Kousouros is the founder and principal of the Law Office of James Kousouros. His career began in 1985 after a coveted clerkship at the Appellate Division, Third Department in Albany, New York where he was responsible for writing draft opinions for the Justices on a wide array of cases. After completing his clerkship, Mr. Kousouros served as an Assistant District Attorney in Bronx County. Thereafter, over twenty-eight years ago, he opened his criminal defense practice. Since then, Mr. Kousouros has handled thousands of criminal matters in the state and federal courts throughout the United States.</p>
+<h3>A distinguished reputation</h3>
+<p>During his years of practice, Mr. Kousouros has earned a distinguished reputation for providing and maintaining the highest levels of professional and ethical standards. He is also known for his meticulous preparation, and is highly regarded as a fierce and passionate trial attorney. As a result of what has been termed his “exemplary” legal representation, it is Mr. Kousouros’ distinct honor to have received the highest of accolades from members of the federal and state judiciary, as well as prosecuting authorities. Indeed, his cross-examinations have been hailed as textbook worthy — and have even prompted a noted Brooklyn law professor to use a transcript of one as a primary source.</p> 
+<p>In addition, Mr. Kousouros is highly regarded as a forceful, innovative, and effective negotiator who is always armed with both a full command of the facts of his case, and a creative manifest of all possible legal options. So armed, he enters negotiations with an insightful analysis of the prosecution’s evidence. This analysis is tailored to expose the weaknesses in the prosecutor’s case, to place his client in a position of strength, and to open the door to novel resolutions.</p>  
+<h3>Strong Relationships with Prosecutors</h3> 
+<p>Mr. Kousouros also makes it a practice to maintain strong working relationships with his adversaries. He does this not only because he places a high value on professionalism and respects his adversaries as colleagues, but also because he knows that those relationships—together with his reputation as a formidable advocate—translate into better outcomes for his clients.</p>
+<h3>Sought after legal expert</h3>
+<p>Over the years Mr. Kousouros has been sought out as a source for his legal expertise on criminal matters by several different media organizations. Indeed, he has been a commentator on major television networks such as Fox News, Discovery Channel—Investigations, Channel 7 Eyewitness News and CW 11, and is currently a weekly legal commentator on the RNN news show hosted by the Emmy Award winning journalist Richard French. Mr. Kousouros has also been interviewed for his opinion as a legal expert by newspaper organizations such as The New York Times, The Journal News, New York Post and Newsday, among others. <a href="http://localhost:8888/kousouros_law/press-media/">(Press/Media)</a></p>
 
+<h5>TEACHING AND LECTURES</h5>
+<p>Visiting professorship University of Birmingham, Birmingham, England.</p>
+<p>New York Law School</p>
+<p>Brooklyn Law School</p>
+<p>Queens County Bar Association</p>
+<h5>NATIONAL AND INTERNATIONAL CREDENTIALS</h5>
+<p><strong>New York</strong> Practice in all federal courts, the Eastern, Southern, Western and Northern Districts</p>
+<p><strong>Pennsylvania</strong> Western District including the Circuit Courts of Appeals for the First, Second, Fourth and Eleventh Circuits.</p>
+<p>Representation in the federal courts of:<br><strong>Virginia, New Jersey, Connecticut, Pennsylvania, Maryland, Washington D.C., North Carolina, South Carolina, Florida, Puerto Rico, Texas, Arizona, and California.</strong></p>
+<p>Assisted clients in the courts of<br><strong>France, Greece, the Republic of the Philippines, Canada, and Cyprus.</strong></p>
+
+SIDEBAR:
+<h5>TEACHING AND LECTURES</h5>
+<p>Visiting professor at the University of Birmingham, Birmingham, England.</p>
+<p>New York Law School </p>
+<p>Brooklyn Law School</p>
+<p>Queens County Bar Association </p>
+<h5>NATIONAL AND INTERNATIONAL REPRESENTATION</h5>
+<p>Admitted to Practice in all Federal Courts of: <strong>New York Eastern, Southern, Western and Northern Districts</strong></p>
+<p><strong>Pennsylvania</strong> Western District and the Circuit Courts of Appeals for the First, Second, Fourth and Eleventh Circuits.</p>
+<p>Representation in the federal courts of: <strong>Virginia, New Jersey, Connecticut, Pennsylvania, Maryland, Washington D.C., North Carolina, South Carolina, Florida, Puerto Rico, Texas, Arizona, and California.</strong></p>
+<p>Assisted clients in the courts of <strong>France, Greece, the Republic of the Philippines, Canada, and Cyprus.</strong></p>
 
 
 <!-- LIST BY SUBCATEGORY -->
