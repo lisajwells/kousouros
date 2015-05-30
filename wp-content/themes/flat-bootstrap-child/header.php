6e2bdca7d198b41f61498f4b32e9d15ec471053f
Lisa Wells <lisawells999@gmail.com>
2015-05-30 17:23:31 -0400

    removing post title href; beginning flyout search field

--- wp-content/themes/flat-bootstrap-child/header.php
+++ wp-content/themes/flat-bootstrap-child/header.php
@@ -47,26 +47,17 @@
 							</a>
 
 							<!-- from the internets -->
-							<!-- <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 								<label>
+									<span class="screen-reader-text sr-only"><?php _ex( 'Search for:', 'label', 'flat-bootstrap' ); ?></span>
 									<input type="search" 
 					                class="search-field form-control" 
 					                placeholder="Search"  
-					                value="" name="s" 
+					                value="<?php echo esc_attr( get_search_query() ); ?>" 
+					                name="s" 
 					                title="Search efter:">
 								</label>
-								<input type="submit" class="search-submit" value="Search">
-							</form> -->
-
-							<!-- from the theme -->
-							<form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-								<div class="form-group">
-									<label>
-										<span class="screen-reader-text sr-only"><?php _ex( 'Search for:', 'label', 'flat-bootstrap' ); ?></span>
-										<input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'flat-bootstrap' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
-									</label>
 									<input type="submit" class="search-submit btn btn-primary" value="<?php echo esc_attr_x( 'Search', 'submit button', 'flat-bootstrap' ); ?>">
-								</div><!-- .form-group -->
 							</form>
 
 
--- wp-content/themes/flat-bootstrap-child/inc/template-tags.php
+++ wp-content/themes/flat-bootstrap-child/inc/template-tags.php
@@ -18,26 +18,25 @@ if ( ! function_exists( 'xsbf_get_the_excerpt' ) ) :
 add_filter( 'get_the_excerpt', 'xsbf_get_the_excerpt' );
 function xsbf_get_the_excerpt( $excerpt ) {
 
-	// !!!!!!!!!!What if the search result is the category instead of being a post or page in the category?!!!!!!!!!
 
 	if ( in_category( 'testimonials' )) { 
-		$page_to_go_to = 'index.php?page_id=1083#testimonial-'.( get_the_ID() ) . ''; 
+		$page_to_go_to = '1083#testimonial-'.( get_the_ID() ) . ''; 
 	} elseif ( in_category ( 'white-collar-general' )) {
-		$page_to_go_to = 'index.php?page_id=1414'; // notables: white-collar-general 
+		$page_to_go_to = '1414'; // notables: white-collar-general 
 	} elseif ( in_category ( 'healthcare-and-pharmaceutical' )) {
-		$page_to_go_to = 'index.php?page_id=1416'; // notables: white-collar-healthcare-pharmaceutical 
+		$page_to_go_to = '1416'; // notables: white-collar-healthcare-pharmaceutical 
 	} elseif ( in_category ( 'homicide' )) {
-		$page_to_go_to = 'index.php?page_id=2245'; // notables: homicide 
+		$page_to_go_to = '2245'; // notables: homicide 
 	} elseif ( in_category ( array( 'racketeering', 'bribery' ))) {
-		$page_to_go_to = 'index.php?page_id=1418'; // notables: racketeering-bribery 
+		$page_to_go_to = '1418'; // notables: racketeering-bribery 
 	} elseif ( in_category ( 'narcotics' )) {
-		$page_to_go_to = 'index.php?page_id=1420'; // notables: narcotics 
+		$page_to_go_to = '1420'; // notables: narcotics 
 	} elseif ( in_category ( array( 'miscellaneous-notables', 'appeals', 'extradition' ))) {
-		$page_to_go_to = 'index.php?page_id=1422'; // notables: miscellaneous-appeals-extradition 
-	} elseif ( in_category ( array( 'press', 'print', 'daily-news', 'journal-news', 'new-york-times', 'newsday', 'times-of-smithtown', 'tv' ))) {
-		$page_to_go_to = 'index.php?page_id=1080'; // press 
+		$page_to_go_to = '1422'; // notables: miscellaneous-appeals-extradition 
+	} elseif ( in_category( 'press' ) || post_is_in_descendant_category( 73 ) ) { // press: calling a function to check if a post is in a descendant category (so as not to list all subcats here. This won't work for notables, where the parent category is not the page 
+		$page_to_go_to = '1080'; // press 
 	} else {
-		$page_to_go_to = ''; // default calls page (good) or individual post (dont really desire this) 
+		$page_to_go_to = ''; // default calls page if page (good) or single post (dont really desire this but it's a fallback and a test for 2 conditions below) 
 	}
 
 	if ( ! is_attachment() ) {
@@ -47,7 +46,7 @@ function xsbf_get_the_excerpt( $excerpt ) {
 		if ( $page_to_go_to == '' ) {
 			$excerpt .= '<a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __( 'Read More', 'flat-bootstrap' ) . '</a>';
 		} else {
-			$excerpt .= '<a class="read-more" href="'.$page_to_go_to.'">' . __( 'Read More', 'flat-bootstrap' ) . '</a>';
+			$excerpt .= '<a class="read-more" href="index.php?page_id='.$page_to_go_to.'">' . __( 'Read More', 'flat-bootstrap' ) . '</a>';
 		}
 	}
 	return $excerpt;
